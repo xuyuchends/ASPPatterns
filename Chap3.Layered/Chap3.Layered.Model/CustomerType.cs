d1a55a310ec61ab41522c2e076b8518335ea1e04
xuyuchends <xuyuchends@gmail.com>
2014-07-16 22:00:23 +0800

    f

---
+++ Chap3.Layered/Chap3.Layered.Model/CustomerType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chap3.Layered.Model
+{
+    public enum CustomerType
+    {
+        Standred = 0,
+        Trade = 1
+    }
+}
---
+++ Chap3.Layered/Chap3.Layered.Model/DiscountFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chap3.Layered.Model
+{
+    /// <summary>
+    /// 工厂模式
+    /// </summary>
+   public static class DiscountFactory
+    {
+       public static IDiscountStrategy GetDiscountStrategyFor(CustomerType customerType)
+       {
+           switch (customerType)
+           {
+               case CustomerType.Standred:
+                   return new TradeDiscountStrategy();
+               default :
+                   return new NullDiscountStrategy();
+           }
+       }
+    }
+}
