156ec782fd29ff5bfca6963aa116e476012169bc
xuyuchends <xuyuchends@gmail.com>
2014-08-06 17:11:53 +0800

    df

---
+++ Chap5.FactoryPattern/test/KuaiDiFunctory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+   public  class KuaiDiFunctory
+    {
+       public static KuaiDi CreateKuaiDiFunctory(Order order)
+       {
+           if (order.TotalCost > 100)
+           {
+               return new ShunFeng();
+           }
+           else
+           {
+               return new ZhongTong();
+           }
+       }
+    }
+}
---
+++ Chap5.FactoryPattern/test/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+   public  class Order
+    {
+       public decimal TotalCost { get; set; }
+       public decimal WeightInKG { get; set; }
+       public string  CourierTrackingId { get; set; }
+       public string   DispatchAddress { get; set; }
+    }
+}
